e8bec32aa4aaded6765e8026e4190599367ad47c
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-06-07 15:56:58 -0500

    Attempts to update devise with additional fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # To allow new fields through Devise
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, :keys => [:full_name, :preferred_name, :grad_year, :hometown, :summer_internship, :kellogg_memory,:grow_up, :ten_year_goals, :anything_else, :profile_photo])
+
+    devise_parameter_sanitizer.permit(:account_update, :keys => [:full_name, :preferred_name, :grad_year, :hometown, :summer_internship, :kellogg_memory,:grow_up, :ten_year_goals, :anything_else, :profile_photo])
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -172,7 +172,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: "industries#index"
+root to: "users#index"
 
 # CREATE --> done through devise but can I still create a profile like this?
 
