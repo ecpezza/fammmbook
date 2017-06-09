c3b522ff520574a9251adbb27369c0e05423e8b5
Elizabeth C. Pezza <ecpezza@users.noreply.github.com>
2017-06-09 12:58:31 -0500

    MVP2

--- app/models/role.rb
+++ app/models/role.rb
@@ -6,7 +6,6 @@ class Role < ApplicationRecord
 
   validates :function_id, :presence => true
   validates :user_id, :presence => true
-
   validate :atleast_one_is_checked
   def atleast_one_is_checked
     errors.add(:base, "Select either interest or experience, or both") unless interest || experience
